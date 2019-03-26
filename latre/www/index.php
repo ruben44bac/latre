666fc7cef8b752c513d264b2ea76229dbceebc68
rubenek <ruben44bac@gmail.com>
2019-03-26 00:10:17 -0600

    ajustes

--- latre/www/index.php
+++ latre/www/index.php
@@ -4,6 +4,7 @@
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/style" />
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
       <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <title>latre</title>
@@ -33,7 +34,7 @@ body {
 }
 .content {
     margin-left: 6%;
-    height: 90vh;
+    height: auto;
     float: left;
     width: 90%;
 }
@@ -52,7 +53,7 @@ body {
     float: left;
   }
   .item {
-    height: 250px;
+    height: auto;
     color: #000;
     text-align: left;
     padding: 20px 8px 20px 8px;
@@ -61,6 +62,7 @@ body {
     -moz-transition: all 0.5s;
     transition: all 0.5s;
     background: #fff;
+    border-radius: 8px;
     font-family: Monoton;
   }
   
@@ -432,6 +434,101 @@ p a:hover {
         box-shadow: -1px 3px 50px 24px rgba(255,255,255,1);    
     }
   }
+
+
+  
+.bubbly-button {
+  font-family: 'Helvetica', 'Arial', sans-serif;
+  display: inline-block;
+  font-size: 1em;
+  padding: 1em 2em;
+  margin-top: 100px;
+  margin-bottom: 60px;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: #f0f0f0;
+  color: #000;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+  position: relative;
+  transition: transform ease-in 0.1s, box-shadow ease-in 0.25s;
+  box-shadow: 0 2px 25px rgba(255, 0, 130, 0.5);
+}
+.bubbly-button:focus {
+  outline: 0;
+}
+.bubbly-button:before, .bubbly-button:after {
+  position: absolute;
+  content: '';
+  display: block;
+  width: 140%;
+  height: 100%;
+  left: -20%;
+  z-index: -1000;
+  transition: all ease-in-out 0.5s;
+  background-repeat: no-repeat;
+}
+.bubbly-button:before {
+  display: none;
+  top: -75%;
+  background-image: radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, transparent 20%, #ff0081 20%, transparent 30%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, transparent 10%, #ff0081 15%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%);
+  background-size: 10% 10%, 20% 20%, 15% 15%, 20% 20%, 18% 18%, 10% 10%, 15% 15%, 10% 10%, 18% 18%;
+}
+.bubbly-button:after {
+  display: none;
+  bottom: -75%;
+  background-image: radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, transparent 10%, #ff0081 15%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%), radial-gradient(circle, #ff0081 20%, transparent 20%);
+  background-size: 15% 15%, 20% 20%, 18% 18%, 20% 20%, 15% 15%, 10% 10%, 20% 20%;
+}
+.bubbly-button:active {
+  transform: scale(0.9);
+  background-color: #F0F0F0;
+  box-shadow: 0 2px 25px rgba(255, 255, 255, 0.2);
+}
+.bubbly-button.animate:before {
+  display: block;
+  animation: topBubbles ease-in-out 0.75s forwards;
+}
+.bubbly-button.animate:after {
+  display: block;
+  animation: bottomBubbles ease-in-out 0.75s forwards;
+}
+
+@keyframes topBubbles {
+  0% {
+    background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%, 40% 90%, 55% 90%, 70% 90%;
+  }
+  50% {
+    background-position: 0% 80%, 0% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%, 50% 50%, 65% 20%, 90% 30%;
+  }
+  100% {
+    background-position: 0% 70%, 0% 10%, 10% 30%, 20% -10%, 30% 20%, 22% 40%, 50% 40%, 65% 10%, 90% 20%;
+    background-size: 0% 0%, 0% 0%,  0% 0%,  0% 0%,  0% 0%,  0% 0%;
+  }
+}
+@keyframes bottomBubbles {
+  0% {
+    background-position: 10% -10%, 30% 10%, 55% -10%, 70% -10%, 85% -10%, 70% -10%, 70% 0%;
+  }
+  50% {
+    background-position: 0% 80%, 20% 80%, 45% 60%, 60% 100%, 75% 70%, 95% 60%, 105% 0%;
+  }
+  100% {
+    background-position: 0% 90%, 20% 90%, 45% 70%, 60% 110%, 75% 80%, 95% 70%, 110% 10%;
+    background-size: 0% 0%, 0% 0%,  0% 0%,  0% 0%,  0% 0%,  0% 0%;
+  }
+}
+
+.boton-cargar{
+  right: 0;
+  bottom: 0;
+  width: 100%;
+}
+.badge {
+  margin: 14px;
+}
+
 </style>
     </head>
 
@@ -439,12 +536,9 @@ p a:hover {
 
     <?php
       if(isset($_POST['submit'])){
-        $target_dir = 'C:/Users/ruben.baeza/Documents/';
-        echo($target_dir);
+        $target_dir = 'C:/Users/Karen/Desktop/ruben_test/';
         $target_file = $target_dir . basename($_FILES["filepath"]["name"]);
-        echo($target_file);
         $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-        echo($imageFileType);
         move_uploaded_file($_FILES["filepath"]["tmp_name"], $target_file);
       }
     ?>
@@ -452,12 +546,11 @@ p a:hover {
             <img src="./logo.png" class="img-res">
         </div>
         <div>
-          <form action="index.php" method="post" enctype="multipart/form-data">
-              Select image to upload:
-              <input type="file" name="filepath" id="fileToUpload">
-              <input type="submit" value="Upload Image" name="submit">
-          </form>
+          
         </div>
+        <?php
+        if(isset($_POST['submit'])){
+        ?>
         <div class="row">
             <div class="col s12 containers"  align="center">
                 <div class="content" align="center">
@@ -554,6 +647,16 @@ p a:hover {
             <a class="btn-floating btn-large waves-effect waves-light red bton-float"><i class="material-icons">add</i></a>
 
         </div>
+
+
+        <?php 
+        }
+        ?>
+          <div class="clearfix"></div>
+          <form class="boton-cargar" align="center" action="index.php" method="post" enctype="multipart/form-data">
+              <input class="bubbly-button" type="file" name="filepath" id="fileToUpload">
+              <input class="bubbly-button" type="submit" value="Cargar" name="submit">
+          </form>
       <!--JavaScript at end of body for optimized loading-->
 
     </body>
